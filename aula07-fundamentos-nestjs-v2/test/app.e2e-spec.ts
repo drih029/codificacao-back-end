84146e9dd98fee183f2f93cd63f829680c859c08
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-09-17 13:35:58 -0300

    test: fix app e2e spec

--- aula07-fundamentos-nestjs-v2/test/app.e2e-spec.ts
+++ aula07-fundamentos-nestjs-v2/test/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    }).compile();   
 
     app = moduleFixture.createNestApplication();
     await app.init();
